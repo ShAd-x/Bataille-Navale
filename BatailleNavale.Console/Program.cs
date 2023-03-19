128d241e6622fc6054ce51e9d984d7bbcd172b2c
ShAd <alexis.tatarkovic@gmail.com>
2023-03-19 18:21:31 +0100

    wip

--- BatailleNavale.Console/Program.cs
+++ BatailleNavale.Console/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Console;
 
-Console.WriteLine("Hello, World!");
+using Repo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
---
+++ BatailleNavale.Repo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Repo;
+
+public class Repo
+{
+    public string getJsonDatas()
+    {
+        string json = string.Empty;
+        using (System.Net.WebClient wc = new System.Net.WebClient())
+        {
+            wc.Headers.Add("x-functions-key", "lprgi_api_key_2023");
+            json = wc.DownloadString("https://api-lprgi.natono.biz/api/GetConfig");
+        }
+        return json;
+    }
+}
